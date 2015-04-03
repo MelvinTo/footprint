2c378cc66a786f25e7a221eb8189f0af0b630f8d
Melvin Tu <tbb@shn-hatu-8816.cisco.com>
2015-04-03 22:08:22 +0800

    Initial Commit

---
+++ Footprint/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Footprint
+//
+//  Created by Melvin Tu on 4/3/15.
+//  Copyright (c) 2015 Melvin Tu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
